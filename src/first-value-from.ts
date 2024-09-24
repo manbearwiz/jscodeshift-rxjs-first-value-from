a25ea97986d54ac894b76a56df3419d4061b7c3b
Kevin Brey <manbearwiz@gmail.com>
2024-09-23 23:16:08 -0500

    fix(*): don't remove operator imports that are still used

--- src/first-value-from.ts
+++ src/first-value-from.ts
@@ -13,44 +13,36 @@ export default function transform(
 ): string | undefined {
   const j = api.jscodeshift;
   const root = j(file.source);
-
-  // Find all toPromise() calls with a take(1) call in the pipe() call chain and replace them with firstValueFrom()
-
   let firstValueFromAdded = false;
 
-  // biome-ignore lint/complexity/noForEach: <explanation>
   root
     .find(j.CallExpression, {
-      callee: {
-        property: {
-          name: 'toPromise',
-        },
-      },
+      callee: { property: { name: 'toPromise' } },
     })
     .forEach((path: ASTPath<CallExpression>) => {
       const toPromiseCall = path.value.callee;
+
       if (
         j.MemberExpression.check(toPromiseCall) &&
         j.CallExpression.check(toPromiseCall.object) &&
-        j.MemberExpression.check(toPromiseCall.object.callee) &&
-        j.CallExpression.check(toPromiseCall.object.callee.object)
+        j.MemberExpression.check(toPromiseCall.object.callee)
       ) {
         const pipeCall = toPromiseCall.object;
         const takeCall = pipeCall.arguments.find(
           (arg) =>
             j.CallExpression.check(arg) &&
             j.Identifier.check(arg.callee) &&
-            arg.callee.name === 'take' &&
-            j.NumericLiteral.check(arg.arguments[0]) &&
-            arg.arguments[0].value === 1,
-        ) as CallExpression;
+            ((arg.callee.name === 'take' &&
+              j.NumericLiteral.check(arg.arguments[0]) &&
+              arg.arguments[0].value === 1) ||
+              arg.callee.name === 'first'),
+        );
 
-        if (!takeCall) {
-          return;
-        }
+        if (!takeCall) return;
 
-        const takeCallIndex = pipeCall.arguments.indexOf(takeCall);
-        pipeCall.arguments.splice(takeCallIndex, 1);
+        pipeCall.arguments = pipeCall.arguments.filter(
+          (arg) => arg !== takeCall,
+        );
 
         const firstValueFromCall = j.callExpression(
           j.identifier('firstValueFrom'),
@@ -62,52 +54,49 @@ export default function transform(
         );
 
         firstValueFromAdded = true;
-
         j(path).replaceWith(firstValueFromCall);
       }
     });
 
   if (firstValueFromAdded) {
-    // add import statement for firstValueFrom
-    const rxjsImport = j.importDeclaration(
-      [j.importSpecifier(j.identifier('firstValueFrom'))],
-      j.literal('rxjs'),
-    );
-
-    root.get().node.program.body.unshift(rxjsImport);
+    root
+      .get()
+      .node.program.body.unshift(
+        j.importDeclaration(
+          [j.importSpecifier(j.identifier('firstValueFrom'))],
+          j.literal('rxjs'),
+        ),
+      );
   }
 
-  // biome-ignore lint/complexity/noForEach: <explanation>
-  root
-    .find(j.ImportDeclaration, {
-      source: {
-        value: 'rxjs/operators',
-      },
-    })
-    .forEach((importPath) => {
-      const importDeclaration = importPath.value;
-      const takeImport = importDeclaration.specifiers?.find((specifier) => {
-        return (
-          j.ImportSpecifier.check(specifier) &&
-          j.Identifier.check(specifier.imported) &&
-          specifier.imported.name === 'take'
+  for (const operator of ['first', 'take']) {
+    root
+      .find(j.ImportDeclaration, { source: { value: 'rxjs/operators' } })
+      .forEach((importPath) => {
+        const importDeclaration = importPath.value;
+
+        if (
+          root.find(j.CallExpression, {
+            callee: { name: operator },
+          }).length
+        ) {
+          return;
+        }
+
+        const remainingSpecifiers = importDeclaration.specifiers?.filter(
+          (specifier) =>
+            j.ImportSpecifier.check(specifier) &&
+            j.Identifier.check(specifier.imported) &&
+            specifier.imported.name !== operator,
         );
+
+        if (remainingSpecifiers?.length) {
+          importDeclaration.specifiers = remainingSpecifiers;
+        } else {
+          j(importPath).remove();
+        }
       });
-
-      if (!takeImport?.local?.name) {
-        return;
-      }
-
-      const takeImportUsed = root.find(j.CallExpression, {
-        callee: {
-          name: 'take',
-        },
-      }).length;
-
-      if (takeImportUsed === 0) {
-        j(importPath).remove();
-      }
-    });
+  }
 
   return root.toSource();
 }
